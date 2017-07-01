dfcf73c38e4005aed865d2f8848a20f6a29e7cc5
Shane Howley <shane@mainframe.com>
2017-07-01 18:47:09 +0100

    Initial commit

---
+++ lib/validix.ex
@@ -0,0 +1,18 @@
+defmodule Validix do
+  @moduledoc """
+  Documentation for Validix.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Validix.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
